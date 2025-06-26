86b52847ba7af1b36f0f1fe2772f62457cc9f7f3
kaziabulhasib <hasibkazi420@gmail.com>
2025-06-26 17:10:48 +0530

    nabar : account : small device

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -79,11 +79,31 @@ const Navbar = () => {
             Seller Dashboard
           </button>
         )}
-        <button className='flex items-center gap-2 hover:text-gray-900 transition'>
-          onClick={openSignIn}
-          <Image src={assets.user_icon} alt='user icon' />
-          Account
-        </button>
+        {user ? (
+          <>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action
+                  label='Cart'
+                  labelIcon={<CartIcon />}
+                  onClick={() => router.push("/cart")}
+                />
+                <UserButton.Action
+                  label='My Orders'
+                  labelIcon={<BagIcon />}
+                  onClick={() => router.push("/my-orders")}
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className='flex items-center gap-2 hover:text-gray-900 transition'>
+            <Image src={assets.user_icon} alt='user icon' />
+            Account
+          </button>
+        )}
       </div>
     </nav>
   );
