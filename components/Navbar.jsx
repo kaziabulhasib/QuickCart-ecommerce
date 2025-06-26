121e42a41c5d4ec06d6dcf1366f03660e5aa507a
kaziabulhasib <hasibkazi420@gmail.com>
2025-06-26 17:16:49 +0530

    small device menu complete

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { assets, BagIcon, CartIcon } from "@/assets/assets";
+import { assets, BagIcon, BoxIcon, CartIcon, HomeIcon } from "@/assets/assets";
 import Link from "next/link";
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
@@ -84,10 +84,21 @@ const Navbar = () => {
             <UserButton>
               <UserButton.MenuItems>
                 <UserButton.Action
+                  label='Home'
+                  labelIcon={<HomeIcon />}
+                  onClick={() => router.push("/")}
+                />
+                <UserButton.Action
+                  label='Products'
+                  labelIcon={<BoxIcon />}
+                  onClick={() => router.push("/all-products")}
+                />
+                <UserButton.Action
                   label='Cart'
                   labelIcon={<CartIcon />}
                   onClick={() => router.push("/cart")}
                 />
+
                 <UserButton.Action
                   label='My Orders'
                   labelIcon={<BagIcon />}
